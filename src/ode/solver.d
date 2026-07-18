75a56b802fd7376171a4fc201c1d7b5450c818de
arawi_kohei <arawi_kohei@ARAWI-Kohei-PC.home>
2026-07-18 09:00:56 +0900

    dub.json is added.

--- src/ode/solver.d
+++ src/ode/solver.d
@@ -1,13 +1,65 @@
+/****************************************************************************
+ * ODE solvers
+ *
+ * Version 1.1
+ *
+ * LICENSE:
+ *     $(LINK2 www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ *
+ * Authors:
+ *     新井浩平 (ARAI Kohei)
+ ****************************************************************************/
 module ode.solver;
 
+import std.traits: isFloatingPoint;
 import ode.plot;
+
+@safe pure nothrow @nogc:
 /*************************************************************
- * Classical Runge-Kutta method
+ * Classical Runge-Kutta method for first-order ordinary differential equations
  *
  * Params:
- *  R= type of floating point
- *  Order= order of the ode
+ *  f= function ddot(x) = f(t, x)
+ *  tInit= initial time
+ *  tMax= terminal time
+ *  xInit= initial value of x
+ *  h= time step
+ *************************************************************/
+ScatterPlot!(R, Order) odeRungeKutta4(R, int Order)(in R function(R, R) f,
+	in R tInit,
+	in R tMax,
+	in R xInit,
+	in R h)
+if(isFloatingPoint!R && Order == 1){
+	import std.numeric: dotProduct;
+	enum CurveOdr= 4;
+	enum R[CurveOdr] weight= [1.0, 2.0, 2.0, 1.0];
+
+	R x= xInit;
+	R[CurveOdr] k;
+	auto result= ScatterPlot!(R, Order)();
+
+	result.append(tInit, xInit);
+
+	for(R t= tInit+h; t <= tMax; t += h){
+		k[0]= f(t, x);
+		k[1]= f(t+h/2, x+h*k[0]/2);
+		k[2]= f(t+h/2, x+h*k[1]/2);
+		k[3]= f(t+h, x+h*k[2]);
+
+		x += h/6.0*dotProduct(k, weight);
+
+		result.append(t, x);
+	}
+
+	return result;
+}
+
+
+/*************************************************************
+ * Classical Runge-Kutta method for second-order ordinary differential equations
  *
+ * Params:
  *  f= function ddot(x) = f(t, x, dot(x)) 
  *  tInit= initial time
  *  tMax= terminal time
@@ -21,7 +73,7 @@ ScatterPlot!(R, Order) odeRungeKutta4(R, int Order)(in R function(R, R, R) f,
 	in R xInit,
 	in R vInit,
 	in R h)
-if(Order == 2){
+if(isFloatingPoint!R && Order == 2){
 	import std.numeric: dotProduct;
 	enum CurveOdr= 4;
 	enum R[CurveOdr] weight= [1.0, 2.0, 2.0, 1.0];
@@ -54,35 +106,3 @@ if(Order == 2){
 
 	return result;
 }
-
-/// ditto
-ScatterPlot!(R, Order) odeRungeKutta4(R, int Order)(in R function(R, R) f,
-	in R tInit,
-	in R tMax,
-	in R xInit,
-	in R h)
-if(Order == 1){
-	import std.numeric: dotProduct;
-	enum CurveOdr= 4;
-	enum R[CurveOdr] weight= [1.0, 2.0, 2.0, 1.0];
-
-	R x= xInit;
-	R[CurveOdr] k;
-	auto result= ScatterPlot!(R, Order)();
-
-	result.append(tInit, xInit);
-
-	for(R t= tInit+h; t <= tMax; t += h){
-		k[0]= f(t, x);
-		k[1]= f(t+h/2, x+h*k[0]/2);
-		k[2]= f(t+h/2, x+h*k[1]/2);
-		k[3]= f(t+h, x+h*k[2]);
-
-		x += h/6.0*dotProduct(k, weight);
-
-		result.append(t, x);
-	}
-
-	return result;
-}
- 
